74261d2398d3832004a5c4d0b92b533e026d5e6d
diakuzena <diakuzena@gmail.com>
2022-01-07 15:06:15 +0100

    Acrescentando a funcionalidde do search

--- chat.php
+++ chat.php
@@ -1,13 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Realtime Chat App | DiakuZena</title>
-        <link rel="stylesheet" href="css/style.css">
-        <link rel="stylesheet" href="css/all.min.css">
-    </head>
+<?php include_once "header.php" ?>
+
     <body>
         <div class="wrapper">
             <section class="chat-area">
--- users.php
+++ users.php
@@ -1,94 +1,45 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Realtime Chat App | DiakuZena</title>
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/all.min.css">
-</head>
+<?php
+    session_start();
+
+    if(!isset($_SESSION["unique_id"])){
+        header("location: login.php");
+    }
+?>
+
+<?php include_once "header.php" ?>
+
 <body>
     <div class="wrapper">
         <section class="users">
             <header>
+
+            <?php
+                include_once "php/config.php";
+
+                $sql = mysqli_query($conn, "SELECT * FROM users WHERE unique_id={$_SESSION['unique_id']}");
+                if(mysqli_num_rows($sql) > 0){
+                    $row = mysqli_fetch_assoc($sql);
+
+
+                }
+            ?>
+
                 <div class="content">
-                    <img src="img/sorriso 1.jpg" alt="">
+                    <img src="php/images/<?= $row['image'] ?>" alt="">
                     <div class="details">
-                        <span>Love Developers</span>
-                        <p>Active now</p>
+                        <span><?= $row['fname'] . " " . $row['lname'] ?></span>
+                        <p><?= $row['status'] ?></p>
                     </div>
                 </div>
                 <a href="#" class="logout">Logout</a>
             </header>
+
             <div class="search">
                 <span class="text">Select an user to start chat</span>
-                <input type="text" placeholder="Enter name to search...">
+                <input type="text" name="serachTerm" placeholder="Enter name to search...">
                 <button><i class="fas fa-search"></i></button>
             </div>
-            <div class="users-list">
-                <a href="#">
-                    <div class="content">
-                        <img src="img/Verdinha 2.jpg" alt="">
-                        <div class="details">
-                            <span>Love Developers</span>
-                            <p>This is a test message</p>
-                        </div>
-                    </div>
-                    <div class="status-dot"><i class="fas fa-circle"></i></div>
-                </a>
-                <a href="#">
-                    <div class="content">
-                        <img src="img/Verdinha 2.jpg" alt="">
-                        <div class="details">
-                            <span>Love Developers</span>
-                            <p>This is a test message</p>
-                        </div>
-                    </div>
-                    <div class="status-dot"><i class="fas fa-circle"></i></div>
-                </a>
-                <a href="#">
-                    <div class="content">
-                        <img src="img/Verdinha 2.jpg" alt="">
-                        <div class="details">
-                            <span>Love Developers</span>
-                            <p>This is a test message</p>
-                        </div>
-                    </div>
-                    <div class="status-dot"><i class="fas fa-circle"></i></div>
-                </a>
-                <a href="#">
-                    <div class="content">
-                        <img src="img/Verdinha 2.jpg" alt="">
-                        <div class="details">
-                            <span>Love Developers</span>
-                            <p>This is a test message</p>
-                        </div>
-                    </div>
-                    <div class="status-dot"><i class="fas fa-circle"></i></div>
-                </a>
-                <a href="#">
-                    <div class="content">
-                        <img src="img/Verdinha 2.jpg" alt="">
-                        <div class="details">
-                            <span>Love Developers</span>
-                            <p>This is a test message</p>
-                        </div>
-                    </div>
-                    <div class="status-dot"><i class="fas fa-circle"></i></div>
-                </a>
-                <a href="#">
-                    <div class="content">
-                        <img src="img/Verdinha 2.jpg" alt="">
-                        <div class="details">
-                            <span>Love Developers</span>
-                            <p>This is a test message</p>
-                        </div>
-                    </div>
-                    <div class="status-dot"><i class="fas fa-circle"></i></div>
-                </a>
-
-            </div>
+            <div class="users-list"></div>
         </section>
     </div>
 
